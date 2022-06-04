87323a4aa835ce6ff92ee7ad92dc694e6a3c8dea
Mateusz Czapliński <czapkofan@gmail.com>
2022-06-04 23:08:15 +0200

    more tests & refactor them a bit

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,13 @@ mod tests {
 
     use std::io::{self, Read};
 
-    fn signature_4<R: Read>(r: R) -> impl Iterator<Item = io::Result<signature::BlockSignature>> {
+    fn signature_4byte<R: Read>(
+        r: R,
+    ) -> impl Iterator<Item = io::Result<signature::BlockSignature>> {
         signature::<R, 4>(r)
     }
 
-    fn delta_4<S, D>(signatures: S, data: D) -> io::Result<Vec<delta::Command>>
+    fn delta_4byte<S, D>(signatures: S, data: D) -> io::Result<Vec<delta::Command>>
     where
         S: IntoIterator<Item = signature::BlockSignature>,
         D: Read,
@@ -24,24 +26,36 @@ mod tests {
         delta::<S, D, 4>(signatures, data)
     }
 
+    fn quick_delta_4byte(old_file: Vec<u8>, new_file: Vec<u8>) -> Vec<delta::Command> {
+        let signature: Vec<_> = signature_4byte(&*old_file)
+            .map(|result| result.unwrap())
+            .collect();
+        delta_4byte(signature, &*new_file).unwrap()
+    }
+
     #[test]
     fn test_delta() {
         use delta::Command::*;
         let old_file = vec![1, 2, 3, 4, 10, 20, 30, 40];
         let new_file = vec![0, 1, 10, 20, 30, 40, 99, 1, 2, 3, 4, 55];
-        let signature: Vec<_> = signature_4(&*old_file)
-            .map(|result| result.unwrap())
-            .collect();
-        let delta = delta_4(signature, &*new_file).unwrap();
-        assert_eq!(
-            delta,
-            [
-                Raw { data: vec![0, 1] },
-                CopyBlock { index: 1 },
-                Raw { data: vec![99] },
-                CopyBlock { index: 0 },
-                Raw { data: vec![55] },
-            ]
-        );
+        let delta = quick_delta_4byte(old_file, new_file);
+        let expected_delta = [
+            Raw { data: vec![0, 1] },
+            CopyBlock { index: 1 },
+            Raw { data: vec![99] },
+            CopyBlock { index: 0 },
+            Raw { data: vec![55] },
+        ];
+        assert_eq!(delta, expected_delta);
+    }
+
+    #[test]
+    fn test_delta_overlapping() {
+        use delta::Command::*;
+        let old_file = vec![1, 1, 1, 1];
+        let new_file = vec![1, 1, 1, 1, 1];
+        let delta = quick_delta_4byte(old_file, new_file);
+        let expected_delta = [CopyBlock { index: 0 }, Raw { data: vec![1] }];
+        assert_eq!(delta, expected_delta);
     }
 }
